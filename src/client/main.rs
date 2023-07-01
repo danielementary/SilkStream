53db1476ed7af53f233321c8a99e837ba0caae25
Daniel Filipe Nunes Silva <danielementary@proton.me>
2023-07-02 01:42:36 +0200

    add basic client code

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,18 @@
+use std::net::UdpSocket;
+
 fn main() {
     println!("Hello, client!");
+
+    let socket = UdpSocket::bind("127.0.0.1:8081").expect("Failed to bind socket");
+
+    println!(
+        "About to send a message on {}",
+        socket.local_addr().unwrap()
+    );
+
+    let buf = "Hey there!".as_bytes();
+
+    socket
+        .send_to(buf, "127.0.0.1:8080")
+        .expect("Failed to send message");
 }
