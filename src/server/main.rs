9166802eef9c6bb3468e9238441b6ff754673aeb
Daniel Filipe Nunes Silva <danielementary@proton.me>
2023-07-02 01:42:29 +0200

    add basic server code

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,23 @@
+use std::net::UdpSocket;
+
 fn main() {
     println!("Hello, server!");
+
+    let socket = UdpSocket::bind("127.0.0.1:8080").expect("Failed to bind socket");
+
+    println!("Listening on {}", socket.local_addr().unwrap());
+
+    let mut buf = [0u8; 1024];
+
+    loop {
+        let (bytes_read, client_addr) = socket
+            .recv_from(&mut buf)
+            .expect("Failed to read from socket");
+
+        let received_message = &buf[..bytes_read];
+        println!(
+            "Received message from {}: {:?}",
+            client_addr, received_message
+        );
+    }
 }
